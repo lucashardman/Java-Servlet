bcab8ca872dbd0212e425d3d269491adc5cc3ba4
Lucas Hardman <lucas.hardman92@gmail.com>
2021-04-23 13:46:11 -0300

    controller ajustado

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -9,12 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import br.com.alura.gerenciador.acoes.AlteraEmpresa;
-import br.com.alura.gerenciador.acoes.ListaEmpresas;
-import br.com.alura.gerenciador.acoes.MostraEmpresa;
-import br.com.alura.gerenciador.acoes.NovaEmpresa;
-import br.com.alura.gerenciador.acoes.NovaEmpresaForm;
-import br.com.alura.gerenciador.acoes.RemoveEmpresa;
+import br.com.alura.gerenciador.acoes.Acao;
 
 /**
  * Servlet implementation class UnicaEntradaServlet
@@ -24,28 +19,19 @@ public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		String paramAcao = request.getParameter("acao");
-		String rdirect = null;
-		if (paramAcao.equals("ListaEmpresas")) {
-			ListaEmpresas acao = new ListaEmpresas();
+		String rdirect;
+		
+		try {
+			Class classe = Class.forName("br.com.alura.gerenciador.acoes."+paramAcao);
+			Acao acao = (Acao) classe.newInstance();
 			rdirect = acao.executa(request, response);
-		} else if (paramAcao.equals("RemoveEmpresa")) {
-			RemoveEmpresa acao = new RemoveEmpresa();
-			rdirect = acao.executa(request, response);
-		} else if (paramAcao.equals("MostraEmpresa")) {
-			MostraEmpresa acao = new MostraEmpresa();
-			rdirect = acao.executa(request, response);
-		} else if (paramAcao.equals("AlteraEmpresa")) {
-			AlteraEmpresa acao = new AlteraEmpresa();
-			rdirect = acao.executa(request, response);
-		} else if (paramAcao.equals("NovaEmpresa")) {
-			NovaEmpresa acao = new NovaEmpresa();
-			rdirect = acao.executa(request, response);
-		} else if (paramAcao.equals("NovaEmpresaForm")) {
-			NovaEmpresaForm acao = new NovaEmpresaForm();
-			rdirect = acao.executa(request, response);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ServletException
+				| IOException e) {
+			throw new ServletException(e);
 		}
+		
 		
 		String[] fwd = rdirect.split(":");
 		
